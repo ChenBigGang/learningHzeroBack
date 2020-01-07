c8600b1fae7338f559191d9c89f5ea9f150d4dd1
Ganggang <gang.chen01@hand-china.com>
2020-01-07 20:45:20 +0800

    gateway依赖错误修复

--- learning-hzero-gateway/src/main/java/com/learning/hzero/gateway/LearningHzeroGatewayApplication.java
+++ learning-hzero-gateway/src/main/java/com/learning/hzero/gateway/LearningHzeroGatewayApplication.java
@@ -1,20 +1,27 @@
 package com.learning.hzero.gateway;
 
 import org.hzero.autoconfigure.gateway.EnableHZeroGateway;
+import org.hzero.gateway.GatewayApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableHZeroGateway
 @EnableDiscoveryClient
-@SpringBootApplication(
-        exclude = {SecurityAutoConfiguration.class}
-)
 public class LearningHzeroGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LearningHzeroGatewayApplication.class, args);
+        //SpringApplication.run(LearningHzeroGatewayApplication.class, args);
+        try {
+            new SpringApplicationBuilder(GatewayApplication.class)
+                    .web(WebApplicationType.REACTIVE)
+                    .run(args);
+        }catch (Throwable e){
+            e.printStackTrace();
+        }
     }
 
 }
