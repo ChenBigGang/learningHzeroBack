a98cb275216cb9140917bfd12317361bcb6d2fb9
Ganggang <gang.chen01@hand-china.com>
2020-01-08 14:03:40 +0800

    版本颠覆

--- learning-hzero-register/src/main/java/com/learning/hzero/register/LearningHzeroRegisterApplication.java
+++ learning-hzero-register/src/main/java/com/learning/hzero/register/LearningHzeroRegisterApplication.java
@@ -3,13 +3,14 @@ package com.learning.hzero.register;
 import org.hzero.autoconfigure.register.EnableHZeroRegister;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.netflix.eureka.server.EurekaServerAutoConfiguration;
 import org.springframework.context.annotation.Import;
 
 @EnableHZeroRegister
 @EnableEurekaServer
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @Import({EurekaServerAutoConfiguration.class})
 public class LearningHzeroRegisterApplication {
 
